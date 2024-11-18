27454d55eb0d2e4b986c8762a684933078432120
sakshi2427 <sakshikulshrestha234@gmail.com>
2024-11-18 17:54:11 +0530

    changes1

--- WebAngularRAC/obj/Debug/net46/win7-x86/WebAngularRAC.AssemblyInfo.cs
+++ WebAngularRAC/obj/Debug/net46/win7-x86/WebAngularRAC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAngularRAC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+321a549a5a3a657c46c343278d995635cbd93f7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b9e6fa86fb7337f4c869208e8d24e23fd88a0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAngularRAC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAngularRAC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
